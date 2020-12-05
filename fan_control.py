04a12a23999ce66d9c90a3ed6e4a419fea892e10
Yauhen <exserial@gmail.com>
2020-12-04 21:02:38 -0800

    Improving fan control script
    * Linear fan speed control function changed to exponential
    * Fewer probings when temperature goes down (WAIT_TIME_TEMP_DOWN and WAIT_TIME_TEMP_UP)
    * Separate parameters for minimum temperature to turn the fan on (MIN_TEMP_ON) and to turn off (MIN_TEMP_OFF)

--- fan_control.py
+++ fan_control.py
@@ -7,47 +7,62 @@ import sys
 import os
 
 # Configuration
-FAN_PIN = 18            # BCM pin used to drive PWM fan
-WAIT_TIME = 1           # [s] Time to wait between each refresh
-PWM_FREQ = 25000        # [Hz] 25kHz for Noctua PWM control
+FAN_PIN = 18                          # BCM pin used to drive PWM fan
+PWM_FREQ = 25000                      # [Hz] 25kHz for Noctua PWM control
+WAIT_TIME_TEMP_UP = 5                 # [s] Time to wait between each temperature probe when temperature goes up
+WAIT_TIME_TEMP_DOWN = 20              # [s] Time to wait between each temperature probe when temperature goes down
 
 # Configurable temperature and fan speed
-MIN_TEMP = 40
+MIN_TEMP_OFF = 30
+MIN_TEMP_ON = 35
 MAX_TEMP = 70
-FAN_LOW = 40
+FAN_LOW = 30
 FAN_HIGH = 100
 FAN_OFF = 0
 FAN_MAX = 100
 
 # Get CPU's temperature
 def getCpuTemperature():
-    res = os.popen('vcgencmd measure_temp').readline()
-    temp =(res.replace("temp=","").replace("'C\n",""))
-    #print("temp is {0}".format(temp)) # Uncomment for testing
+    res = os.popen("vcgencmd measure_temp").readline()
+    temp = res.replace("temp=", "").replace("'C\n", "")
+    #print("Temp = {0}".format(temp))
     return temp
 
 # Set fan speed
 def setFanSpeed(speed):
+    global previousFanSpeed
+    if speed > FAN_MAX:
+        speed = FAN_MAX
+    elif speed < FAN_OFF:
+        speed = FAN_OFF
     fan.start(speed)
+    previousFanSpeed = speed
+    #print("Speed = {0}".format(speed))
     return()
 
 # Handle fan speed
 def handleFanSpeed():
+    global waitTime
+    global previousTemp
     temp = float(getCpuTemperature())
-    # Turn off the fan if temperature is below MIN_TEMP
-    if temp < MIN_TEMP:
-        setFanSpeed(FAN_OFF)
-        #print("Fan OFF") # Uncomment for testing
+    # Turn off the fan if temperature is below MIN_TEMP_OFF
+    if temp < MIN_TEMP_OFF:
+        speed = FAN_OFF
+    # Set fan speed to LOW if it is already spinning and temperature is below MIN_TEMP_ON
+    if previousFanSpeed > FAN_OFF and temp < MIN_TEMP_ON:
+        speed = FAN_LOW
     # Set fan speed to MAXIMUM if the temperature is above MAX_TEMP
     elif temp > MAX_TEMP:
-        setFanSpeed(FAN_MAX)
-        #print("Fan MAX") # Uncomment for testing
+        speed = FAN_HIGH
     # Caculate dynamic fan speed
     else:
-        step = float(FAN_HIGH - FAN_LOW)/float(MAX_TEMP - MIN_TEMP)  
-        temp -= MIN_TEMP
-        setFanSpeed(FAN_LOW + ( round(temp) * step ))
-        #print(FAN_LOW + ( round(temp) * step )) # Uncomment for testing
+        speed = FAN_LOW + round((FAN_HIGH - FAN_LOW) * ((temp - MIN_TEMP_ON) ** 2) / ((MAX_TEMP - MIN_TEMP_ON) ** 2))
+    setFanSpeed(speed)
+    if previousTemp > temp:
+        waitTime = WAIT_TIME_TEMP_DOWN
+    else:
+        waitTime = WAIT_TIME_TEMP_UP
+    previousTemp = temp
     return ()
 
 try:
@@ -55,13 +70,16 @@ try:
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(FAN_PIN, GPIO.OUT, initial=GPIO.LOW)
-    fan = GPIO.PWM(FAN_PIN,PWM_FREQ)
+    fan = GPIO.PWM(FAN_PIN, PWM_FREQ)
+    waitTime = WAIT_TIME_TEMP_UP
+    previousTemp = MIN_TEMP_OFF
+    previousFanSpeed = FAN_OFF
     setFanSpeed(FAN_OFF)
-    # Handle fan speed every WAIT_TIME sec
+    # Handle fan speed every 'waitTime' sec
     while True:
         handleFanSpeed()
-        time.sleep(WAIT_TIME)
+        #print("Sleeping for {0}s...".format(waitTime))
+        time.sleep(waitTime)
 
 except KeyboardInterrupt: # trap a CTRL+C keyboard interrupt
     setFanSpeed(FAN_HIGH)
-    #GPIO.cleanup() # resets all GPIO ports used by this function
